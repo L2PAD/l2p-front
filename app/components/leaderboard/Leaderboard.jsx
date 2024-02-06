4efc5d2b35343d67a110e70593a64288a78da00b
Dima Boiko <boikod887@gmail.com>
2024-02-06 14:25:00 +0200

    leaderboard label

--- app/components/leaderboard/Leaderboard.jsx
+++ app/components/leaderboard/Leaderboard.jsx
@@ -2,13 +2,14 @@ import { useState, useEffect } from "react"
 import { useSelector } from "react-redux"
 import { getLeaderBoardData } from '../../smart/initialSmartMain'
 import Info from "../../assets/components/info/Info"
+import ClosedPageLabel from '../../assets/components/closedPageLabel/ClosedPageLabel'
 import MyScore from "../../assets/components/myScore/MyScore"
 import LeaderboardTable from "../../assets/components/leaderboardTable/LeaderboardTable"
 import ProjectsStats from "../../assets/components/projectsStats/ProjectsStats"
 import Hidden from "../../assets/components/HiddenComponent/Hidden"
-import styles from '../styles/leaderboard.module.scss'
 import useProjects from "../../hooks/useProjects"
 import Loader from '../../assets/components/loader/Loader'
+import styles from '../styles/leaderboard.module.scss'
 
 export default function Leaderboard() {
   const userData = useSelector((state) => state.auth.userData)
@@ -55,6 +56,13 @@ export default function Leaderboard() {
         </div>
         <div className={styles.projectStats}>
           <ProjectsStats projects={allProjects}/>
+        </div>
+        <div className={styles.closed}>
+          <ClosedPageLabel
+          text={'Activity leaderboard '}
+          description={'Stake & Invite friends to rank up'}
+          label={'Soon'}
+          />
         </div>
     </div>
     :
